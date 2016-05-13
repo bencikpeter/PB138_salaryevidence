75ba9df80804475335bac94a6c32e4def5af1846
olda <oldik.kon@gmail.com>
2016-05-13 13:13:40 +0200

    added DatabaseException, DatabaseManager interface, DayRecord class, WorkType enum..

--- SalaryEvidence/database/src/main/java/DatabaseManager.java
+++ SalaryEvidence/database/src/main/java/DatabaseManager.java
@@ -1,5 +1,39 @@
+import java.util.List;
+
 /**
- * Created by olda on 11.05.2016.
+ * Interface for DatabaseManager
+ * Created by oldrichkonecny on 13.05.16.
  */
-public class DatabaseManager {
+public interface DatabaseManager {
+
+    /**
+     * Stores new record into database, id is generated automatically.
+     * @param dayRecord record to be stored.
+     * @throws DatabaseFailureException when db operation fails.
+     */
+    void createRecord(DayRecord dayRecord) throws DatabaseFailureException;
+
+    /**
+     * Update record in database, if record is not found in database, nothing happen.
+     * @param dayRecord record to be updated.
+     * @throws DatabaseFailureException when db operation fails.
+     */
+    void updateRecord(DayRecord dayRecord) throws DatabaseFailureException;
+
+    /**
+     * Delete record in database, if record is not found, nothing happen.
+     * @param dayRecord record to be deleted.
+     * @throws DatabaseFailureException when db operation fails.
+     */
+    void deleteRecord(DayRecord dayRecord) throws DatabaseFailureException;
+
+    /**
+     * Delete record in database, if record is not found, nothing happen.
+     * @param unixDate date of record to be deleted.
+     * @throws DatabaseFailureException when db operation fails.
+     */
+    void deleteRecord(Long unixDate) throws DatabaseFailureException;
+
+
+    List<DayRecord> findRecord(Long id) throws DatabaseFailureException;
 }
