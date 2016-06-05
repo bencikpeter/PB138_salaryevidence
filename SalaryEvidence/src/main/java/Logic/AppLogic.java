f2f8938ef27c92c1e24a29c8f158b1b4de73319f
Peter <p.tirala@atlas.sk>
2016-06-05 17:39:31 +0200

    Merge

--- SalaryEvidence/src/main/java/Logic/AppLogic.java
+++ SalaryEvidence/src/main/java/Logic/AppLogic.java
@@ -1,14 +1,13 @@
 package Logic;
 
+import java.nio.file.Path;
+import java.util.List;
+
 /**
  *Interface for application logic. 
  * There are functions for GUI, which combine database and transformations.
  * @author Peter Tirala
  */
 public interface AppLogic {
-    void transoformDoc(int from,int to/*source from DB */);
-    
-    void transformPdf(int from,int to/*source from DB*/);
-    
-    void createXML(Jobs job,Day[] day);
+    public Path createInvoice (List<Day> days);
 }
--- SalaryEvidence/src/main/java/Logic/Day.java
+++ SalaryEvidence/src/main/java/Logic/Day.java
@@ -1,12 +1,17 @@
 package Logic;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
+
 /**
  * Object represents one working day.
  * @author Peter Tirala
  */
 public class Day {
     private int hours;
-    private int date; // int just for now
+    private LocalDate date; 
+    private Jobs job;
 
     public int getHours() {
         return hours;
@@ -16,13 +21,47 @@ public class Day {
         this.hours = hours;
     }
 
-    public int getDate() {
+    public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(int date) {
+    public void setDate(LocalDate date) {
         this.date = date;
     }
+
+    public Jobs getJob() {
+        return job;
+    }
+
+    public void setJob(Jobs job) {
+        this.job = job;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.date);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Day other = (Day) obj;
+        if (!Objects.equals(this.date, other.date)) {
+            return false;
+        }
+        return true;
+    }
+   
     
     
 }
