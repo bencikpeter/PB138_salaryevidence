07b1cff8eae636dcc9dd50e8f076f4d1ae22918d
bencikpeter <bencik.peter.ml@gmail.com>
2016-06-06 14:55:18 +0200

    Transformations changed from interface to utility class, getNewInstanceXmlSchemaValidator implemented

--- SalaryEvidence/src/main/Transformations.java
+++ SalaryEvidence/src/main/Transformations.java
@@ -1,13 +1,28 @@
 package main;
 
+import org.xml.sax.SAXException;
+
+import java.io.File;
+
 /**
  * Created by bencikpeter on 09.05.16.
  */
-public interface Transformations {
-    XmlSchemaValidator getNewInstanceXmlSchemaValidator(/*XML SCHEMA FILE*/);
+public final class Transformations {
 
-    XmlToDocbookTransformation getNewInstanceXmlToDocbook();
+    private Transformations(){
+        throw new UnsupportedOperationException("Transformations is utility class - cannot be instantiated");
+    }
 
-    DocbookToPdfTransformation getNewInstanceDocbookToPdf();
+    public static XmlSchemaValidator getNewInstanceXmlSchemaValidator(File schema) throws SAXException {
+        return new XmlSchemaValidatorImpl(schema);
+    }
+
+    public static XmlToDocbookTransformation getNewInstanceXmlToDocbook(){
+        return null;
+    }
+
+    public static DocbookToPdfTransformation getNewInstanceDocbookToPdf(){
+        return null;
+    }
 }
 
