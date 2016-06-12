ca58317259a1d882aa0d39276f7502d4c163c3d5
olda <oldik.kon@gmail.com>
2016-06-12 20:13:05 +0200

    DaySAXHandler changed

--- SalaryEvidence/database/src/main/java/DaySAXHandler.java
+++ SalaryEvidence/database/src/main/java/DaySAXHandler.java
@@ -8,14 +8,14 @@ import org.xml.sax.helpers.DefaultHandler;
  * Created by olda on 12.06.2016.
  */
 public class DaySAXHandler extends DefaultHandler {
+
     private Day day = null;
+    private boolean bHours;
+    private boolean bJob;
 
     public Day getDay() {
         return this.day;
     }
-
-    private boolean bHours;
-    private boolean bJob;
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
