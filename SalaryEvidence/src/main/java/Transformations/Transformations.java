c942193068f86f9e2ba3c63acddfcb7f0fa8536b
bencikpeter <bencik.peter.ml@gmail.com>
2016-06-13 23:48:35 +0200

    hot fix

--- SalaryEvidence/src/main/java/Transformations/Transformations.java
+++ SalaryEvidence/src/main/java/Transformations/Transformations.java
@@ -42,7 +42,7 @@ public final class Transformations {
      * @return new instance of DocbookToPdfTransformation
      */
     public static DocbookToPdfTransformation getNewInstanceDocbookToPdf(){
-        return null;
+        return new DocbookToPdfTransformationImpl();
     }
 }
 
