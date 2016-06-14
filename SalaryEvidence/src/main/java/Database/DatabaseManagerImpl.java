0bfea88f9636f745194340411cdefd3bcc4ce339
Peter <p.tirala@atlas.sk>
2016-06-14 09:10:23 +0200

    merge

--- SalaryEvidence/src/main/java/Database/DatabaseManagerImpl.java
+++ SalaryEvidence/src/main/java/Database/DatabaseManagerImpl.java
@@ -35,7 +35,7 @@ public class DatabaseManagerImpl implements DatabaseManager{
     private static final long DAY_LENGTH = 86400L;
     private static final String FILE_EXTENSION = ".xml";
     //private static final String CONFIG_PATH = "SalaryEvidence/database/src/main.java/config.properties";
-    private static final String CONFIG_PATH = "SalaryEvidence/src/main/java/Database/config.properties";
+    private static final String CONFIG_PATH = "src/main/java/Database/config.properties";
     
     private static String URI;
     private static String recordsCollectionPath;
